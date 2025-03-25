3abb80cd2d204b2c6c3986b097aaa2b36af22f57
DEEPAN . B <deepanb2005@gmail.com>
2025-03-25 22:59:50 +0530

    Update DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/feedback_system";
-    private static final String USER = "root"; // Replace with your MySQL username
+    private static final String USER = "root";// Replace with your MySQL username
     private static final String PASSWORD = ""; // Replace with your MySQL password
 
     public static Connection getConnection() throws SQLException {
